1136a8e8151e7e1f022eb9896b17ffc63b2d961c
Thorben <t.jezioro@gmx.de>
2017-05-12 13:38:08 +0200

    Update Gui.cs

--- Gui.cs
+++ Gui.cs
@@ -1,27 +1,54 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Webcrawler
 {
     class Gui
     {
+        public static string ReadLine(string message)
+        {
+            Gui.Write(message);
+            var input = Console.ReadLine();
+            Gui.WriteLine();
+
+            return input;
+        }
+
+        public static void WriteLine(string text = "")
+        {
+            Console.WriteLine(text);
+        }
+
+        public static void Write(string text)
+        {
+            Console.Write(text);
+        }
+
+        public static void WriteRed(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Gui.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        public static void WriteGreen(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Gui.WriteLine(text);
+            Console.ResetColor();
+        }
+
         public static string UserRequest()
         {
             string userRequest = Console.ReadLine();
             return userRequest;
         }
-        public static void WriteLine(string text)
-        {
-            Console.WriteLine(text);
-        }
-        public static void Write(string text)
-        {
-            Console.Write(text);
-        }
+
         public static void WriteList(List<string> list)
         {
             list.ForEach(i => Console.Write("{0}\n", i));
         }
+
         public static int UserYN()
         {
             string userRequest = System.Console.ReadLine();
@@ -37,22 +64,6 @@ namespace Webcrawler
             {
                 return 3;
             }
-        }
-        public static void ResetColor()
-        {
-            Console.ResetColor();
-        }
-        public static void WriteFontRed(string text)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Gui.WriteLine(text);
-            Gui.ResetColor();
-        }
-        public static void WriteFontGreen(string text)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Gui.WriteLine(text);
-            Gui.ResetColor();
         }
     }
 }
